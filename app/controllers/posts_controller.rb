7a32643be836fd3095d1baf24a9b72398c4f44b5
mutai-gilbert <mutaigilbert85@gmail.com>
2023-06-15 16:50:24 +0300

    Add new files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,7 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id])
   end
-
   def show
-    @posts = Post.where(author_id: params[:user_id], id: params[:id]).first
+    @post = Post.where(author_id: params[:user_id], id: params[:id]).first
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+   # create users
+   first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1560250097-0b93528c311a', bio: 'Teacher from Mexico.')
+   second_user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330', bio: 'Teacher from Poland.')
+
+   # create posts
+   first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+   second_post = Post.create(author: second_user, title: 'I am changing the world!', text: 'This is my second post')
+   third_post = Post.create(author: second_user, title: 'Coding in progress', text: 'One line at a time')
+   fourth_post = Post.create(author: first_user, title: 'Show love', text: 'You cant love too much')
+
+    # create comments
+    Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+    Comment.create(post: first_post, author: first_user, text: 'Hi Lilly!' )
+    Comment.create(post: first_post, author: second_user, text: 'Hope you all done with your project?' )
+    Comment.create(post: first_post, author: second_user, text: 'I asked after you from Ebun yesterday' )
+    Comment.create(post: first_post, author: first_user, text: 'Oh, really, thanks.' )
+    Comment.create(post: first_post, author: second_user, text: 'I am almost done with the project' )
+
+    # likes
+    Like.create(post: first_post, author: first_user)
+    Like.create(post: first_post, author: second_user)
+    Like.create(post: second_post, author: first_user)
