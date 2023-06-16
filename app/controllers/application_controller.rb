07254fbbb383ec5ab30d0e1583d386b3fc943624
mutai-gilbert <mutaigilbert85@gmail.com>
2023-06-16 19:49:44 +0300

    Add new files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    User.find_by_id(1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:show] do
     resources :posts, only: [:index, :show]
+  end
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
   end
 end
