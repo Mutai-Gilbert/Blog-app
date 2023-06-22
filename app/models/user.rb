a0ffc81c2deabbdfdf16f94b0ae46ae169cb3c34
mutai-gilbert <mutaigilbert85@gmail.com>
2023-06-22 17:10:45 +0300

    Add new files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :post, foreign_key: 'author_id'
