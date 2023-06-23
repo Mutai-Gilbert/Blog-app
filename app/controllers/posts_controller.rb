34da262a5b20e86d9a334007cbb0491186521c65
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-06-23 11:42:14 +0200

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end 
+
   private
 
   def post_params
